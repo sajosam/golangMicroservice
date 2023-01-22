950df41ddb2d6a16cf1a6a6b5bf9cd410b37ae76
sajosam <sajo2023b@mca.ajce.in>
2023-01-22 11:48:38 +0530

    90% added

--- mainmicro/main.go
+++ mainmicro/main.go
@@ -11,12 +11,19 @@ import (
 
 func main() {
 	var usrhandlerobj userdb.UsrHandler
-	usrhandlerobj.Connection("localhost","postgres","root","forgolang","5433")
+	usrhandlerobj.Connection("host.docker.internal","postgres","root","forgolang","5433")
 	router:=mux.NewRouter()
 	router.HandleFunc("/health", userdb.HealthCheck).Methods("GET")
 	router.HandleFunc("/user", usrhandlerobj.GetUser).Methods("GET")
 	router.HandleFunc("/adduser", usrhandlerobj.AddUser).Methods("POST")
+	router.HandleFunc("/ordHome", OrdHome).Methods("GET")
 	router.HandleFunc("/invHome", InvHome).Methods("GET")
+	router.HandleFunc("/proHome", ProHome).Methods("GET")
+	router.HandleFunc("/pro/{id}", SinglePro).Methods("GET")
+	router.HandleFunc("/delInv/{id}", DelInv).Methods("DELETE")
+	router.HandleFunc("/addInv", addInv).Methods("POST")
+	router.HandleFunc("/addPro", addPro).Methods("POST")
+
 
 	http.Handle("/", router)
 	http.ListenAndServe(":8400", router)
@@ -32,9 +39,67 @@ func InvHome(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	// w.WriteHeader(http.StatusOK)
-	// json.NewEncoder(w).Encode(resp.Body())
 	w.Write([]byte(resp.Body()))
-	// w.Write([]byte(resp.String()))
-	// w.Write(resp.Body())
+}
+
+func OrdHome(w http.ResponseWriter, r *http.Request) {
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8300/order")
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
+}
+
+func ProHome(w http.ResponseWriter, r *http.Request) {
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8100/product")
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
+}
+
+func SinglePro(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8100/product/"+vars["id"])
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
+}
+
+func DelInv(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	client := resty.New()
+	resp, err := client.R().Delete("http://localhost:8200/delinventory/"+vars["id"])
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
+}
+
+func addInv(w http.ResponseWriter, r *http.Request) {
+	client := resty.New()
+	resp, err := client.R().Post("http://localhost:8200/addinventory")
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
+}
+
+func addPro(w http.ResponseWriter, r *http.Request) {
+	client := resty.New()
+	resp, err := client.R().Post("http://localhost:8100/addproduct")
+	// print the values in the response
+	if err != nil {
+		panic(err)
+	}
+	w.Write([]byte(resp.Body()))
 }
